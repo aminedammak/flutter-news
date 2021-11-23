9570afb630ae03860444bf5a9074e462a72489ed
adammak <amine.dammak@habemus.com>
2021-11-23 16:57:32 +0100

    Added test cases for fetching items

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -13,8 +13,8 @@ class NewsApiProvider {
     return ids;
   }
 
-  fetchItem() async {
-    final response = await client.get(Uri.parse("$_root/item/8863.json"));
+  fetchItem(int id) async {
+    final response = await client.get(Uri.parse("$_root/item/$id.json"));
 
     final parsedJson = json.decode(response.body);
 
