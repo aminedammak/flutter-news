23dfd1915e83371391ffdb00b17a4bd562481566
adammak <amine.dammak@habemus.com>
2021-11-26 18:11:23 +0100

    Default item properties when null

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -2,28 +2,28 @@ import 'dart:convert';
 
 class ItemModel {
   final int id;
-/*   final bool deleted; */
+  final bool deleted;
   final String type;
   final String by;
   final int time;
-  /*  final String text;
+  final String text;
   final bool dead;
-  final int parent; 
-  final List<dynamic> kids;*/
+  final int parent;
+  final List<dynamic> kids;
   final String url;
   final int score;
   final String title;
   final int descendants;
   ItemModel.fromJson(Map<String, dynamic> parsedJson)
       : id = parsedJson["id"],
-        /* deleted = parsedJson["deleted"], */
+        deleted = parsedJson["deleted"] ?? false,
         type = parsedJson["type"],
         by = parsedJson["by"],
         time = parsedJson["time"],
-        /* text = parsedJson["text"],
-        dead = parsedJson["dead"],
-        parent = parsedJson["parent"], 
-        kids = parsedJson["kids"],*/
+        text = parsedJson["text"] ?? '',
+        dead = parsedJson["dead"] ?? false,
+        parent = parsedJson["parent"] ?? 0,
+        kids = parsedJson["kids"] ?? [],
         url = parsedJson["url"],
         score = parsedJson["score"],
         title = parsedJson["title"],
@@ -31,14 +31,14 @@ class ItemModel {
 
   ItemModel.fromDb(Map<String, dynamic> parsedJson)
       : id = parsedJson["id"],
-        /* deleted = parsedJson["deleted"] == 1, */
+        deleted = parsedJson["deleted"] == 1,
         type = parsedJson["type"],
         by = parsedJson["by"],
         time = parsedJson["time"],
-        /* text = parsedJson["text"],
+        text = parsedJson["text"],
         dead = parsedJson["dead"] == 1,
         parent = parsedJson["parent"],
-        kids = json.decode(parsedJson["kids"]),*/
+        kids = json.decode(parsedJson["kids"]),
         url = parsedJson["url"],
         score = parsedJson["score"],
         title = parsedJson["title"],
@@ -47,14 +47,14 @@ class ItemModel {
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       "id": id,
-      /* "deleted": deleted ? 1 : 0, */
+      "deleted": deleted ? 1 : 0,
       "type": type,
       "by": by,
       "time": time,
-      /* "text": text,
+      "text": text,
       "dead": dead ? 1 : 0,
       "parent": parent,
-      "kids": json.encode(kids),*/
+      "kids": json.encode(kids),
       "url": url,
       "score": score,
       "title": title,
