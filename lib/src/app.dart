a0817167c0241da8e38606812311a74028c88904
adammak <amine.dammak@habemus.com>
2021-12-06 21:03:40 +0100

    Pass the id of the item to the detail screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'screens/news_list.dart';
+import 'screens/new_details.dart';
 import "blocs/stories_provider.dart";
 
 class App extends StatelessWidget {
@@ -8,8 +9,26 @@ class App extends StatelessWidget {
     return StoriesProvider(
       child: MaterialApp(
         title: "News",
-        home: NewsList(),
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == "/") {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+    } else {
+      return MaterialPageRoute(
+        builder: (context) {
+          //A fantastic location to do some initialization
+          final itemId = int.parse(settings.name!.replaceFirst("/", "", 0));
+          return NewsDetails(itemId: itemId);
+        },
+      );
+    }
   }
 }
